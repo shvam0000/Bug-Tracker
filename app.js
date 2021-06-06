25b1df56183ef690ea74bff8d1ef9e85bb94550b
Shivam Shekhar <shvam0000@gmail.com>
2021-06-06 16:58:23 +0530

    ✨ added comments for reference

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const MongoClient = require('mongodb').MongoClient;
 var db
 var user = null;
 
+//for file uploads
+//multer --> node module for file uploads
 var storage = multer.diskStorage({
     diskStorage: function(req, file, cb) {
         cb(null, 'public/files/')
@@ -23,6 +25,7 @@ const upload = multer({
     storage: storage
 })
 
+//database connection
 const uri = 'mongodb+srv://admin:admin@node-rest-shop.5kcqy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 client.connect(err => {
@@ -30,11 +33,13 @@ client.connect(err => {
     console.log('connected');
 })
 
+//to import templating engine -> EJS
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
+//middleware
 app.get('/', (req, res, next) => {
     res.render('index');
 })
@@ -51,6 +56,7 @@ app.get('/login', (req, res, next) => {
     res.render('login', {x: ''})
 })
 
+//to fetch the data from DB and display it
 app.get('/main', (req, res, next) => {
     db.collection('items').find().toArray((err, result) => {
         if(err) return console.log(err)
@@ -61,17 +67,21 @@ app.get('/main', (req, res, next) => {
     })
 })
 
+//save user creds in the database
 app.post('/signup', (req, res, next) => {
     db.collection('signup').save(req.body, (err, result) => {
+        //error handling
         if (err) {
             res.status(302).redirect('/signup');
             console.log(err);
         }
+        //if no error
         console.log('saved to database');
         res.status(302).redirect('/login');    
     })
 })
 
+//this triggers when you submit the login form
 app.post('/logindata', (req, res, next) => {
     db.collection('signup').findOne({username: req.body.username, password: req.body.password}, (err, result) => {
         if(err) {
@@ -89,6 +99,7 @@ app.post('/logindata', (req, res, next) => {
     })
 })
 
+//to store the data in DB
 app.post('/add', (req, res, next) => {
     db.collection('items').insertOne(req.body.item, (err, result) => {
         if (err) {
@@ -101,15 +112,18 @@ app.post('/add', (req, res, next) => {
     })
 })
 
+//to logout the user
 app.post('/logout', (req, res, next) => {
     user = null;
     res.redirect('/login');
 })
 
+//page not found error
 app.get('*', (req, res, next) => {
     res.status(404).render('404');
 })
 
+//to start the server
 app.listen(process.env.PORT || 3000, () => {
     console.log('listening on 3000');
 })
